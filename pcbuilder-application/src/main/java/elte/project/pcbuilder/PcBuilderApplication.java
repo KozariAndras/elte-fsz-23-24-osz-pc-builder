2f2da6d99fe43771c677255567265bea571d0589
András Kozári <pordnb@INF.ELTE.HU>
2023-11-15 23:27:16 +0100

    H2 implementation halfway done

--- pcbuilder-application/src/main/java/elte/project/pcbuilder/PcBuilderApplication.java
+++ pcbuilder-application/src/main/java/elte/project/pcbuilder/PcBuilderApplication.java
@@ -1,11 +1,24 @@
 package elte.project.pcbuilder;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PcBuilderApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(PcBuilderApplication.class,args);
+    public static void main(String[] args) {SpringApplication.run(PcBuilderApplication.class,args);}
+
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx){
+        return args -> {
+            System.out.println("Hello!");
+
+        };
     }
+
+
+
 }
--- pcbuilder-domain/src/main/java/domain/classes/Cart.java
+++ pcbuilder-domain/src/main/java/domain/classes/Cart.java
@@ -5,7 +5,6 @@ import java.util.List;
 public class Cart {
 
     private int id;
-
     private List<PCComponent> pcComponents;
 
 
