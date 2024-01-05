6b2738d62e097456848b05ae1a8e6a7377f2ae59
Daniel Barbat <barbat.daniel16@gmail.com>
2024-01-05 18:27:03 +0100

    Adds UserService with DTO

---
+++ pcbuilder-application/src/main/java/elte/project/pcbuilder/service/UserService.java
@@ -0,0 +1,40 @@
+package elte.project.pcbuilder.service;
+
+import elte.project.pcbuilder.domain.DTOs.UserDto;
+import elte.project.pcbuilder.domain.enums.Role;
+import elte.project.pcbuilder.domain.user.Credential;
+import elte.project.pcbuilder.domain.user.User;
+import elte.project.pcbuilder.exception.UserAlreadyExistException;
+import elte.project.pcbuilder.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class UserService implements IUserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public User RegisterNewUser(UserDto userDto) throws UserAlreadyExistException {
+        if (usernameExists(userDto.getUsername())){
+        throw new UserAlreadyExistException("The username " + userDto.getUsername() + " already exists.");
+        }
+        User user = new User();
+        Credential credential = new Credential();
+
+        credential.setUsername(userDto.getUsername());
+        credential.setPassword(userDto.getPassword());
+        user.setCredential(credential);
+
+        user.setRole(Role.USER);
+
+        return userRepository.save(user);
+    }
+
+    private boolean usernameExists(String username) {
+        return userRepository.findUserByCredentialUsername(username).isPresent();
+    }
+}
